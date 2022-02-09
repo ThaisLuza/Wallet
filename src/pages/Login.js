dc8f3cd6bb53842bfd41038d95872777f0b637c1
Thais Luza <thaisluza21@hotmail.com>
2022-02-09 14:18:40 -0300

    requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Input from '../components/Input';
+import Button from '../components/Button';
 
-class Login extends React.Component {
+class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      senha: '',
+      isDisabled: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.onSubmitForm = this.onSubmitForm.bind(this);
+  }
+
+  onSubmitForm() {
+    const { history } = this.props;
+    history.push('/carteira');
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, senha, isDisabled } = this.state;
+    return (
+      <fieldset>
+        <Input
+          data-testid="email-input"
+          label="email: "
+          type="email"
+          onChange={ this.handleChange }
+          value={ email }
+          name="email"
+          required
+        />
+        <Input
+          data-testid="password-input"
+          minlength="6"
+          label="senha: "
+          type="password"
+          onChange={ this.handleChange }
+          value={ senha }
+          name="senha"
+          required
+        />
+        <Button
+          type="submit"
+          label="Entrar"
+          disabled={ isDisabled }
+          onClick={ this.onSubmitForm }
+        />
+      </fieldset>
+    );
   }
 }
-
 export default Login;
