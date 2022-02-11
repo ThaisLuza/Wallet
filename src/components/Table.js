bfb6bc117d747e106dd5992ee07f44753705a8a4
Thais Luza <thaisluza21@hotmail.com>
2022-02-11 18:19:13 -0300

    requisito 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -23,26 +23,26 @@ class Table extends Component {
         </thead>
 
         <tbody>
-          {expenses.map((item) => {
-            return (
-              <tr key={ item.id }>
-                <td>{item.description}</td>
-                <td>{item.tag}</td>
-                <td>{item.method}</td>
-                <td>{item.despesa}</td>
-                <td>{item.exchangeRates[item.currency].name}</td>
-                <td>{Number(item.exchangeRates[item.currency].ask).toFixed(2)}</td>
-                <td>
-                  {(
-                    Number(item.value)
-                    * Number(item.exchangeRates[item.currency].ask)
-                  ).toFixed(2)}
-                </td>
-                <td>Real</td>
-                <td>edita/excluir</td>
-              </tr>
-            );
-          })}
+          {expenses.map((item) => (
+            <tr key={ item.id }>
+              <td>{item.description}</td>
+              <td>{item.tag}</td>
+              <td>{item.method}</td>
+              <td>{item.despesa}</td>
+              <td>{item.exchangeRates[item.currency].name}</td>
+              <td>
+                {Number(item.exchangeRates[item.currency].ask).toFixed(2)}
+              </td>
+              <td>
+                {(
+                  Number(item.value)
+                  * Number(item.exchangeRates[item.currency].ask)
+                ).toFixed(2)}
+              </td>
+              <td>Real</td>
+              <td>edita/excluir</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     );
